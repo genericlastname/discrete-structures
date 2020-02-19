b7a6ee64562770a871830105a1cbd2f646a190a3
Kyle Brown <brownkylej@gmail.com>
2020-02-19 11:24:15 -0500

    Start calendar project

---
+++ calendar/calendar.cc
@@ -0,0 +1,11 @@
+#include "calendar.h"
+using namespace std;
+
+// ===================================================================
+// Print a line of dashes of n length
+// ===================================================================
+void print_line(int n) {
+    for (int i = 0; i < n; i++)
+        cout << "-";
+    cout << endl;
+}
